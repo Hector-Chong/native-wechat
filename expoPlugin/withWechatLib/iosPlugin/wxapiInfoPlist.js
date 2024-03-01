f1409ed4d4030f9f5377fea6e9414d44664aa456
Kara X_X <jameszhan02@hotmail.com>
2024-02-29 16:05:27 -0800

    remove wxid

--- expoPlugin/withWechatLib/iosPlugin/wxapiInfoPlist.js
+++ expoPlugin/withWechatLib/iosPlugin/wxapiInfoPlist.js
@@ -4,12 +4,6 @@ var config_plugins_1 = require("@expo/config-plugins");
 var wxapiInfoPlist = function (config) {
     return (0, config_plugins_1.withInfoPlist)(config, function (config) {
         // Add or modify entries in the Info.plist as needed
-        config.modResults.CFBundleURLTypes = [
-            {
-                CFBundleURLName: 'weixin',
-                CFBundleURLSchemes: ['wx91390954a8079ed3'], // Replace with your actual WeChat app ID
-            },
-        ];
         config.modResults.LSApplicationQueriesSchemes = ['weixin', 'wechat', 'weixinULAPI'];
         return config;
     });
--- expoPlugin/withWechatLib/iosPlugin/wxapiInfoPlist.ts
+++ expoPlugin/withWechatLib/iosPlugin/wxapiInfoPlist.ts
@@ -4,13 +4,6 @@ import { withInfoPlist } from '@expo/config-plugins'
 const wxapiInfoPlist: ConfigPlugin = (config) => {
   return withInfoPlist(config, (config) => {
     // Add or modify entries in the Info.plist as needed
-    config.modResults.CFBundleURLTypes = [
-      {
-        CFBundleURLName: 'weixin',
-        CFBundleURLSchemes: ['wx91390954a8079ed3'], // Replace with your actual WeChat app ID
-      },
-    ]
-
     config.modResults.LSApplicationQueriesSchemes = ['weixin', 'wechat', 'weixinULAPI']
 
     return config
